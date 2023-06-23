ded9b86ece098fde7074034bf465eb90c2812fad
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-06-24 04:10:45 +0530

    manage storage and clear cache

--- app/src/main/java/com/example/filemanagement/Directorys.kt
+++ app/src/main/java/com/example/filemanagement/Directorys.kt
@@ -1,13 +1,23 @@
 package com.example.filemanagement
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.storage.StorageManager
+import android.os.storage.StorageManager.ACTION_CLEAR_APP_CACHE
+import android.os.storage.StorageManager.ACTION_MANAGE_STORAGE
+import androidx.annotation.RequiresApi
+import androidx.core.content.getSystemService
 import androidx.lifecycle.lifecycleScope
 import com.example.filemanagement.databinding.ActivityDirectorysBinding
 import kotlinx.coroutines.launch
+import java.util.UUID
 
 class Directorys : AppCompatActivity() {
-    private  lateinit var  binding : ActivityDirectorysBinding
+    private lateinit var binding: ActivityDirectorysBinding
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDirectorysBinding.inflate(layoutInflater)
@@ -38,5 +48,27 @@ class Directorys : AppCompatActivity() {
             }
         }
 
+        binding.buttonEmptySpace.setOnClickListener() {
+            lifecycleScope.launch {
+                val storageManager = applicationContext.getSystemService<StorageManager>()!!
+                val appSpecificInternalDirUuid: UUID = storageManager.getUuidForPath(filesDir)
+                val freeSpace = storageManager.getAllocatableBytes(appSpecificInternalDirUuid)
+                binding.textViewShowFilesDir.text = (freeSpace / (1024 * 1024)).toString()
+            }
+        }
+        binding.buttonManageStorage.setOnClickListener() {
+            val manageIntent: Intent = Intent().apply {
+                action = ACTION_MANAGE_STORAGE
+            }
+            startActivity(manageIntent)
+        }
+
+        binding.buttonClearCache.setOnClickListener() {
+            val intent : Intent = Intent().apply {
+                action = ACTION_CLEAR_APP_CACHE
+            }
+            startActivity(intent)
+        }
     }
+
 }
