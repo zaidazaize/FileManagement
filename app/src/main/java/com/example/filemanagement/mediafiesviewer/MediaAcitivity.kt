b5d709de5adaf6571fa0ac59a428f41dffc1057f
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-06-25 14:55:55 +0530

    media files viewer Initialization

---
+++ app/src/main/java/com/example/filemanagement/mediafiesviewer/MediaAcitivity.kt
@@ -0,0 +1,12 @@
+package com.example.filemanagement.mediafiesviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.filemanagement.R
+
+class MediaAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_acitivity)
+    }
+}
