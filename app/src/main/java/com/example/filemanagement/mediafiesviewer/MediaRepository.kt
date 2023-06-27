bd6cfa1f74020b86b25b35c7e32f80df49301414
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-06-28 03:04:59 +0530

    initialize repository class

---
+++ app/src/main/java/com/example/filemanagement/mediafiesviewer/MediaRepository.kt
@@ -0,0 +1,110 @@
+package com.example.filemanagement.mediafiesviewer
+
+import android.app.Application
+import android.content.ContentResolver
+import android.content.ContentUris
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.filemanagement.mediafiesviewer.models.MediaImage
+import com.example.filemanagement.mediafiesviewer.models.MediaVideo
+
+class MediaRepository(private val application: Application) {
+
+    // live data for images
+    private val _images = MutableLiveData<List<MediaImage>>()
+    val images: LiveData<List<MediaImage>>
+        get() = _images
+
+    private val _videos = MutableLiveData<List<MediaVideo>>()
+    val videos: LiveData<List<MediaVideo>>
+        get() = _videos
+
+    // load images from media store
+    fun loadImages() {
+        //collections name of images
+        val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        }  else {
+          MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+        application.contentResolver.query(
+            collection,
+            arrayOf(
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.SIZE
+            ),
+            null,
+            null,
+            "${MediaStore.Images.Media.DATE_MODIFIED} ASC"
+        ).use {cursor->
+            if(cursor != null){
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                val nameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+                val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE)
+                val images = mutableListOf<MediaImage>()
+                Log.d("MediaRepository", "loadImages: ${cursor.count}")
+                while (cursor.moveToNext()){
+                    val id = cursor.getLong(idColumn)
+                    val name = cursor.getString(nameColumn)
+                    val size = cursor.getString(sizeColumn)
+                    val contentUri = ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        id)
+                    val item = MediaImage(id, contentUri, name, size)
+                    images.add(item)
+                }
+                _images.postValue(images)
+            }
+        }
+    }
+
+    // load videos from media store
+    fun loadVideos(){
+        //collections name of videos
+        val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        }  else {
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        }
+        application.contentResolver.query(
+            collection,
+            arrayOf(
+                MediaStore.Video.Media._ID,
+                MediaStore.Video.Media.DISPLAY_NAME,
+                MediaStore.Video.Media.SIZE,
+                MediaStore.Video.Media.DURATION
+            ),
+            null,
+            null,
+            "${MediaStore.Video.Media.DATE_MODIFIED} ASC"
+        ).use {cursor->
+            if(cursor != null){
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+                val nameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
+                val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE)
+                val durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION)
+                val videos = mutableListOf<MediaVideo>()
+                Log.d("MediaRepository", "loadVideos: ${cursor.count}")
+                while (cursor.moveToNext()){
+                    val id = cursor.getLong(idColumn)
+                    val name = cursor.getString(nameColumn)
+                    val size = cursor.getString(sizeColumn)
+                    val duration = cursor.getString(durationColumn)
+                    val contentUri = ContentUris.withAppendedId(
+                        MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                        id)
+                    val item = MediaVideo(contentUri, id, name, size, duration)
+                    videos.add(item)
+                }
+                _videos.postValue(videos)
+            }
+        }
+    }
+
+}
